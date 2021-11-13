1a3a9734746fae36ee0bf07f78fde2a1add2c6da
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2021-11-13 17:11:39 +0600

    colors changed and logo added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
@@ -13,21 +11,37 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: Text("BrotherDev"),
+        flexibleSpace: const Padding(
+          padding: EdgeInsets.only(top: 40),
+          child: Image(
+            image: AssetImage("image/BrotherDev-Logo.png"),
+            height: 40,
+            width: 40,
+            fit: BoxFit.contain,
+          ),
+        ),
+        title: const Text(
+          "BrotherDev",
+        ),
+        backgroundColor: Colors.black26,
       ),
       body: Column(
         //mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
 
-        children: [
-          Center(
-            child: Image(
-              image: AssetImage("image/nahid.jpg"),
-              height: 200,
-              width: 200,
+        children: const [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Image(
+                image: AssetImage("image/nahid.jpg"),
+                height: 200,
+                width: 200,
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
